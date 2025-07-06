f95a7fcdead16ef538784384a9fe5578418465f3
michaelboegner <cmikeb@gmail.com>
2025-07-06 13:52:55 +0700

    added additional route and modified route names for clarity. Functional.

--- app/api.py
+++ app/api.py
@@ -1,11 +1,17 @@
 from fastapi import APIRouter, HTTPException
 from app.model import get_embedding
-from app.schemas import BatchRequest, EmbeddingResponse
+from app.schemas import QueryEmbedRequest, QueryEmbedResponse, ContextEmbedRequest, ContextEmbedResponse
 
 router = APIRouter()
 
-@router.post("/embed_batch", response_model=EmbeddingResponse)
-def embed_batch(req: BatchRequest):
+@router.post("/embed_query", response_model=QueryEmbedResponse)
+def embed_batch(req: QueryEmbedRequest):
+    vector = get_embedding(req.text)
+    return {"embeddings": vector}
+
+
+@router.post("/embed_context", response_model=ContextEmbedResponse)
+def embed_batch(req: ContextEmbedRequest):
     texts = [chunk.text for chunk in req.chunks]
     vectors = get_embedding(texts)
     return {"embeddings": vectors}
--- app/schemas.py
+++ app/schemas.py
@@ -1,13 +1,16 @@
 from pydantic import BaseModel
 from typing import List
 
+class QueryEmbedRequest(BaseModel):
+    text: str
 class EmbedChunk(BaseModel):
     topic_number: int
     question_number: int
     text: str
-
-class BatchRequest(BaseModel):
+class ContextEmbedRequest(BaseModel):
     chunks: List[EmbedChunk]
 
-class EmbeddingResponse(BaseModel):
+class QueryEmbedResponse(BaseModel):
+    embeddings: List[float]
+class ContextEmbedResponse(BaseModel):
     embeddings: List[List[float]]
